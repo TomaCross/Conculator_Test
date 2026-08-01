c04f13816521c837bc3236f303d7fd6e5f86d58d
Toma_Cross <deivharison@gmail.com>
2026-08-01 14:16:35 +0400

    Add files via upload

---
+++ Conculatot_VERSION 2.py
@@ -0,0 +1,18 @@
+print('---Канкулятор---')
+while True:
+    try:
+        num1 = float(input('Ведите первое число\n'))
+    except ValueError:
+        print('Пожалуйста введите цифру!')
+        continue
+    try:
+        operatiom = input('Выберите знак + или -\n')
+    try:
+        num2 = float(input('Ведите второе число\n'))
+    except ValueError:
+        print('Пожалуйста введите цифру!')
+        continue
+    if operatiom == '+':
+        print(num1 + num2)
+    elif operatiom == '-':
+        print(num1 - num2)
